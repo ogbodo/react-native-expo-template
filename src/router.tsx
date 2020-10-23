da293edaf89f9cf3d481aec6293b86babbf255f4
Izuking <matthiasogbodo@gmail.com>
2020-10-23 12:52:29 +0100

    re-organise navigation

--- src/router.tsx
+++ src/router.tsx
@@ -1,60 +1,22 @@
 import React from 'react';
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItemList
-} from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+import Screens from './screens';
 import { NavigationContainer } from '@react-navigation/native';
-import Constants from 'expo-constants';
 
-import HomeNavigator from './navigator/HomeNavigator';
-import { useThemeContext } from './theme';
-import Button from './components/button';
+const HomeStack = createStackNavigator();
 
-const Drawer = createDrawerNavigator();
-
-export default function AppNavigator() {
-  const { colors, fonts } = useThemeContext();
-
+export default function HomeNavigator() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-        initialRouteName="HomeNavigator"
-        drawerContentOptions={{
-          style: { marginTop: Constants.statusBarHeight },
-          activeTintColor: colors.BG_PRIMARY_COLOR,
-          labelStyle: { textTransform: 'uppercase' }
-        }}
-        drawerContent={props => {
-          return (
-            <DrawerContentScrollView {...props}>
-              <DrawerItemList {...props} />
-              <Button
-                title="signin button"
-                onPress={() => props.navigation.navigate('SigninScreen')}
-                textStyle={{
-                  textTransform: 'capitalize',
-                  fontSize: fonts.MEDIUM_SIZE
-                }}
-                buttonStyle={{ marginTop: 40, marginLeft: 10, width: '80%' }}
-              />
-              <Button
-                title="signup button"
-                onPress={() => props.navigation.navigate('SignupScreen')}
-                textStyle={{
-                  textTransform: 'capitalize',
-                  fontSize: fonts.MEDIUM_SIZE
-                }}
-                buttonStyle={{ marginTop: 40, marginLeft: 10, width: '80%' }}
-              />
-            </DrawerContentScrollView>
-          );
-        }}
+      <HomeStack.Navigator
+        initialRouteName="HomeScreen"
+        screenOptions={{ headerShown: false }}
+        mode="modal"
       >
-        <Drawer.Screen name="home" component={HomeNavigator} />
-        <Drawer.Screen name="menu1" component={HomeNavigator} />
-        <Drawer.Screen name="meu2" component={HomeNavigator} />
-      </Drawer.Navigator>
+        <HomeStack.Screen name="HomeScreen" component={Screens.HomeScreen} />
+        <HomeStack.Screen name="SignupScreen" component={Screens.SignupScreen} />
+        <HomeStack.Screen name="SigninScreen" component={Screens.SigninScreen} />
+      </HomeStack.Navigator>
     </NavigationContainer>
   );
 }
